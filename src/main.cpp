04c557d8e8684d2e973bc23beebd82aae117231b
PaoDerDoktor <paoderdoktor@gmail.com>
2019-12-18 14:32:52 +0100

    Added mat render and started gamma correction

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,21 @@
 #include "hitable.h"
 #include "hitable_list.h"
 #include "sphere.h"
-#include "camera.h"
+#include "camera.h" 
+
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    while (p.squared_length() >= 1.0) {
+        p = 2.0*vec3(float(rand()) / RAND_MAX, float(rand()) / RAND_MAX, float(rand()) / RAND_MAX) - vec3(1, 1, 1);
+    }
+    return p;
+}
 
 vec3 color(const ray &r, hitable *world) {
     hit_record rec;
     if (world->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5*vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5*color(ray(rec.p, target - rec.p), world);
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5*(unit_direction.y() + 1.0);
@@ -24,6 +33,7 @@ vec3 color(const ray &r, hitable *world) {
 }
 
 int main(int argc, char *argv[]) {
+    srand(time(NULL));
 
     std::ofstream out("out.ppm");
 
@@ -52,6 +62,8 @@ int main(int argc, char *argv[]) {
                 col += color(r, world);
             }
             col /= float(ns);
+
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 
             int ir = int(255.99*col.r());
             int ig = int(255.99*col.g());
